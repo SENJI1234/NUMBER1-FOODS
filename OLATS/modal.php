ce55da9fdad7e41ac177c4d1b0085adb1f2d7140
SENJI1234 <hakdoghakdog860@gmail.com>
2024-11-21 05:26:33 -0800

    Add files via upload

---
+++ OLATS/modal.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="aaa.css">
+
+<body>
+
+
+        <div class="ones" id="modal">
+            
+            <span class="close" onclick="closeModal()">&times</span>
+            <h1>Order product</h1>
+            <img src="images/apple.png" alt="">
+            <H3>Apple</H3>
+            <h3>Price per piece</h3>
+            <form action="" method="post">
+                
+                <label for="">Customer Name:</label><br>
+                <input type="text"><br>
+                <label for="">Contact information:</label><br>
+                <input type="text"><br>
+                <label for="">Buy Option:</label><br>
+                <select name="" id="">
+                    <option value="">Per Piece</option>
+            <option value="">Per kilo</option>
+        </select>
+    
+            
+            <h3>Total prize $30:00 </h3>
+            <button>Submit Order</button>
+        </div>
+    </div>
+         
+        </form>
+    </div>
+</body>
+</html>
